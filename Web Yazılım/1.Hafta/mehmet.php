7cf10075bd458c49c3c00461fd8849b4be5298a3
Mehmet KANBAZ <mkanbaz@outlook.com>
2021-09-28 10:21:18 +0300

    .

---
+++ Web Yazılım/1.Hafta/mehmet.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+</head>
+<body>
+<?php 
+$tel_no = "0555 555 55 55";
+$sinif = 10;
+
+$a = 5;
+$b = 6;
+$toplam = $a + $b; //toplama değişkeni
+$cikarma = $a - $b; // çıkarma değişkeni
+$carpma = $a * $b; // çarpma değişkeni
+$bolme = $a/$b; //bölme değişkeni
+$bizimsayi = 2;
+//$bizimsayi++;// 1 artırma komutu
+//$bizimsayi--; // 1 azaltma komutu
+
+//$bizimsayi += 5;
+//$bizimsayi *= 5;
+//$bizimsayi -= 5;
+//$bizimsayi /= 5;
+?>
+
+<h1>Toplama Sonucu : <?php echo $toplam ?></h1>
+<h1>Çıkarma Sonucu : <?php echo $cikarma ?></h1>
+<h1>Çarpma Sonucu : <?php echo $carpma ?></h1>
+<h1>Bölme Sonucu : <?php echo $bolme ?></h1>
+<h1><?php echo $bizimsayi ?></h1>
+</body>
+</html>
